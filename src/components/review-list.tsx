9488246fd8ac2a3daf81b878431553916a290da2
Damir <kim2.da@edu.spbstu.ru>
2024-04-07 19:17:48 +0300

    minor fix 2

--- src/components/review-list.tsx
+++ src/components/review-list.tsx
@@ -4,16 +4,15 @@ import Review from './review';
 
 type ReviewListProps = {
   reviews: TReview[];
-  offerId: number;
 };
 
-function ReviewList ({reviews, offerId}: ReviewListProps): JSX.Element {
+function ReviewList ({reviews}: ReviewListProps): JSX.Element {
   return (
     <section className="offer__reviews reviews">
-      <h2 className="reviews__title">Reviews · <span className="reviews__amount">1</span></h2>
+      <h2 className="reviews__title">Reviews · <span className="reviews__amount">{reviews.length}</span></h2>
       <ul className="reviews__list">
         {reviews.map((review) => (
-          <Review key={offerId + review.id} review={review}/>
+          <Review key={review.id} review={review}/>
         ))}
       </ul>
       <CommentForm/>
